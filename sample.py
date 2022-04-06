621e03a010ebf956c129c1ebc4ed67ef2cc7b957
Adithya K <a.dty.3403@gmail.com>
2022-04-06 20:10:41 +0530

    updated

--- sample.py
+++ sample.py
@@ -1,1 +1,12 @@
-p
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        l, r = 0, int(c ** 0.5)
+        while l <= r:
+            p = l ** 2 + r ** 2
+            if p == c:
+                return True
+            elif p < c:
+                l += 1
+            else:
+                r -= 1
+        return False
