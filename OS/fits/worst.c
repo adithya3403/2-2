711781ce420bcf8d17daf1326b82c59c50051233
Adithya K <a.dty.3403@gmail.com>
2022-07-21 21:07:35 +0530

    Update worst.c

--- OS/fits/worst.c
+++ OS/fits/worst.c
@@ -1,44 +1,47 @@
 // Worst Fit
 
 #include <stdio.h>
-void main() {
-    int bsize[20], psize[20];
-    int all[20], m, n, i, j;
+int main() {
+    int b, p, bsize[10], psize[10], all[10], flag[10];
     printf("Enter no of blocks : ");
-    scanf("%d", &m);
+    scanf("%d", &b);
     printf("Enter the size of each block : ");
-    for (i = 0; i < m; i++) {
+    for (int i = 0; i < b; i++) {
         scanf("%d", &bsize[i]);
     }
     printf("Enter the no of processes : ");
-    scanf("%d", &n);
+    scanf("%d", &p);
     printf("Enter the size of each process : ");
-    for (i = 0; i < n; i++) {
+    for (int i = 0; i < p; i++) {
         scanf("%d", &psize[i]);
+    }
+    for (int i = 0; i < p; i++) {
+        flag[i] = 0;
         all[i] = -1;
     }
-    for (int i = 0; i < n; i++) {
-        int worstplace = -1;
-        for (j = 0; j < m; j++) {
-            if (bsize[j] >= psize[i]) {
-                if (worstplace == -1)
-                    worstplace = j;
-                else if (bsize[worstplace] < bsize[j])
-                    worstplace = j;
+    for (int i = 0; i < p; i++) {
+        int worstPlace = -1;
+        for (int j = 0; j < b; j++) {
+            if (bsize[j] >= psize[i] && !flag[j]) {
+                if (worstPlace == -1)
+                    worstPlace = j;
+                else if (bsize[worstPlace] < bsize[j])
+                    worstPlace = j;
             }
         }
-        if (worstplace != -1) {
-            all[i] = worstplace;
-            bsize[worstplace] -= psize[i];
+        if (worstPlace != -1) {
+            all[i] = worstPlace;
+            bsize[worstPlace] -= psize[i];
+            flag[worstPlace] = 1;
         }
     }
-    printf("\n Process No \t Process Size\t Block no.\n");
-    for (int i = 0; i < n; i++) {
-        printf("% d\t\t % d\t\t", i + 1, psize[i]);
+    printf("\nProcess No.\tProcess Size\tBlock no.\n");
+    for (int i = 0; i < p; i++) {
+        printf("%d \t\t %d \t\t", i + 1, psize[i]);
         if (all[i] != -1)
-            printf("% d\n", all[i] + 1);
+            printf("%d\n", all[i] + 1);
         else
-            printf("Not Allocated \n");
+            printf("Not Allocated\n");
     }
 }
 
@@ -52,6 +55,6 @@ Enter the size of each process : 212 417 112 426
 Process No      Process Size    Block no.
 1                212            5
 2                417            2
-3                112            5
+3                112            4
 4                426           Not Allocated
 */
