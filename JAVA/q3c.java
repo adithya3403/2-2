15e3ceff4a53799884479fbcf48732d02c9447fa
Adithya K <a.dty.3403@gmail.com>
2022-08-03 14:02:24 +0530

    rain water

--- JAVA/q3c.java
+++ JAVA/q3c.java
@@ -4,30 +4,21 @@
 // Input : arr[] = {3, 0, 0, 2, 0, 4}
 // Output : 10
 // We can trap"3*2 units"of water between 3 an 2,"1 unit"on top of bar 2 and"3 units"between 2 and 4.
-
 public class q3c {
-    public static int maxWater(int[] arr, int n) {
+    public static int maxWater(int[] arr) {
         int res = 0;
+        int n = arr.length;
         for (int i = 1; i < n - 1; i++) {
             int left = arr[i];
-            for (int j = 0; j < i; j++) {
-                left = Math.max(left, arr[j]);
-            }
+            for (int j = 0; j < i; j++) left = Math.max(left, arr[j]);
             int right = arr[i];
-            for (int j = i + 1; j < n; j++) {
-                right = Math.max(right, arr[j]);
-            }
+            for (int j = i + 1; j < n; j++) right = Math.max(right, arr[j]);
             res += Math.min(left, right) - arr[i];
         }
         return res;
     }
-
     public static void main(String[] args) {
         int[] arr = { 3, 0, 0, 2, 0, 4 };
-        int n = arr.length;
-        System.out.print(maxWater(arr, n));
+        System.out.print(maxWater(arr));
     }
 }
-
-// EXPECTED OUTPUT:
-// 10
