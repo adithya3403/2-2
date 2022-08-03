b0d50799bb6dca408c3f8faf7752cd45ef197632
Adithya K <a.dty.3403@gmail.com>
2022-08-03 14:05:05 +0530

    merge strings

--- JAVA/q4b.java
+++ JAVA/q4b.java
@@ -1,37 +1,24 @@
 // 4 b.Given two strings s1 and s2, your task is to merge those strings to form a new 
 // merged string. A merge operation on two strings is described as follows: Append alternating 
 // characters from s1 and s2,respectively,to mergedString. Once all of the characters in one 
-// of the strings have been merged,append the remaining characters in the other string to 
-// mergedString.
-
+// of the strings have been merged,append the remaining characters in the other string to mergedString.
 import java.util.*;
-class q4b {
+public class q4b {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         String w1 = sc.next();
         String w2 = sc.next();
-        int n1 = w1.length();
-        int n2 = w2.length();
+        int n1 = w1.length(), n2 = w2.length();
         StringBuilder sb = new StringBuilder();
         int i1 = 0, i2 = 0;
         while (i1 < n1 && i2 < n2) {
             sb.append(w1.charAt(i1++));
             sb.append(w2.charAt(i2++));
         }
-        while (i1 < w1.length()) {
-            sb.append(w1.charAt(i1++));
-        }
-        while (i2 < w2.length()) {
-            sb.append(w2.charAt(i2++));
-        }
+        while (i1 < w1.length()) {sb.append(w1.charAt(i1++));}
+        while (i2 < w2.length()) {sb.append(w2.charAt(i2++));}
         System.out.println(sb.toString());
-        sc.close();
     }
 }
-
-// EXPECTED OUTPUT:
-// Sample Input:
-// abc def
-// Sample Output:
-// adbecf
-
+// Sample Input: abc def 
+// Sample Output: adbecf
