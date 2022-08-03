9f8febc75acfda714733697b9dc1dce0fa31c3ae
Adithya K <a.dty.3403@gmail.com>
2022-08-03 22:51:48 +0530

    Update q4a.java

--- JAVA/q4a.java
+++ JAVA/q4a.java
@@ -8,17 +8,16 @@ import java.util.*;
 public class q4a {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        String text1 = sc.next();
-        String text2 = sc.next();
+        String text1 = sc.next(), text2 = sc.next();
         int n1 = text1.length(), n2 = text2.length();
-        int[][] dp = new int[n1 + 1][n2 + 1];
+        int[][] str = new int[n1 + 1][n2 + 1];
         for (int i = 1; i <= n1; i++) {
             for (int j = 1; j <= n2; j++) {
-                if (text1.charAt(i - 1) == text2.charAt(j - 1)) dp[i][j] = dp[i - 1][j - 1] + 1;
-                else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) str[i][j] = str[i - 1][j - 1] + 1;
+                else str[i][j] = Math.max(str[i - 1][j], str[i][j - 1]);
             }
         }
-        System.out.println(dp[n1][n2]);
+        System.out.println(str[n1][n2]);
     }
 }
 // input: abcdefghijk caeghiklp
