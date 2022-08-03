281b7aa52774becbe34f66282694c447729752f4
Adithya K <a.dty.3403@gmail.com>
2022-08-03 13:47:18 +0530

    Update q1a.java

--- JAVA/q1a.java
+++ JAVA/q1a.java
@@ -1,8 +1,6 @@
 // 1a. Given a number, check if the number(N) can be written as the form (k+1)*k.
 // Write a java program to print those numbers in the given range.
-
 import java.util.*;
-
 public class q1a {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -14,10 +12,8 @@ public class q1a {
                 }
             }
         }
-        sc.close();
     }
 }
-
 // Sample Input:10
 // Sample Output:2
 // 6
