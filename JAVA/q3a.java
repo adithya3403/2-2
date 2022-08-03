7c4513125868353ebcf8b66af5ba82364b0e3f45
Adithya K <a.dty.3403@gmail.com>
2022-08-03 13:59:45 +0530

    max diff

--- JAVA/q3a.java
+++ JAVA/q3a.java
@@ -1,30 +1,21 @@
 // 3. a.Given an array arr[]of N integers,the task is to find the maximum difference
 // between any two elements of the array.
-
 import java.util.*;
-
 public class q3a {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] a = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = sc.nextInt();
-        }
+        for (int i = 0; i < n; i++) a[i] = sc.nextInt();
         int diff = 0;
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                if (Math.abs(a[i] - a[j]) > diff) {
-                    diff = Math.abs(a[i] - a[j]);
-                }
+                if (Math.abs(a[i] - a[j]) > diff) diff = Math.abs(a[i] - a[j]);
             }
         }
         System.out.println(diff);
-        sc.close();
     }
 }
-
-// EXPECTED OUTPUT:
 // Sample Input:
 // 5
 // 10 20 30 40 50
