531ea165e54e5d58d224a031ddca4470ac90cef3
Adithya K <a.dty.3403@gmail.com>
2022-08-03 14:24:35 +0530

    fCount( )

--- JAVA/q9a.java
+++ JAVA/q9a.java
@@ -3,9 +3,7 @@
 // count of the given word. For example if the string passed is "hello" the map
 // should return {h-1,e-1,l-2,o-1}. The order of the characters should be same
 // as in the string.
-
 import java.util.*;
-
 public class q9a {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
@@ -21,30 +19,17 @@ public class q9a {
             }
         }
         System.out.println();
-        sc.close();
     }
-
     public static Map<Character, Integer> fCount(String str) {
         Map<Character, Integer> map = new HashMap<Character, Integer>();
         for (int i = 0; i < str.length(); i++) {
-            if (map.containsKey(str.charAt(i))) {
-                map.put(str.charAt(i), map.get(str.charAt(i)) + 1);
-            } else {
-                map.put(str.charAt(i), 1);
-            }
+            if (map.containsKey(str.charAt(i))) map.put(str.charAt(i), map.get(str.charAt(i)) + 1);
+            else map.put(str.charAt(i), 1);
         }
         return map;
     }
 }
-
-/*
-EXPECTED OUTPUT
-
-Run 1:
-Enter a string:hello
-h-1 e-1 l-2 o-1
-
-Run 2:
-Enter a string:kmit
-k-1 m-1 i-1 t-1
-*/
+// Enter a string:hello
+// h-1 e-1 l-2 o-1
+// Enter a string:kmit
+// k-1 m-1 i-1 t-1
