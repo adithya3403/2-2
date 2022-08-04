321792b72a3b6a116978abe8249e449e1ef1b346
Adithya K <a.dty.3403@gmail.com>
2022-08-04 08:20:49 +0530

    Update q_10c.java

--- JAVA/q_10c.java
+++ JAVA/q_10c.java
@@ -22,28 +22,21 @@
 import java.sql.*;
 import java.util.*;
 public class q_10c {
-    public static void main(String args[]) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter the Department No. : ");
-        int dno = input.nextInt();
-        System.out.print("Enter the Department Name : ");
-        String dname = input.next();
-        System.out.print("Enter the Department Location : ");
-        String dloc = input.next();
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/test", "root", "");
-            PreparedStatement pstmt = null;
-            String query = "INSERT INTO dept(deptno, dname, location)" + "VALUES (?, ?, ?)";
-            pstmt = con.prepareStatement(query);
-            pstmt.setInt(1, dno);
-            pstmt.setString(2, dname);
-            pstmt.setString(3, dloc);
-            int status = pstmt.executeUpdate();
-            if (status > 0) System.out.println("Record is inserted successfully !!!");
-            con.close();
-        } catch (Exception e) { System.out.println(e); }
-        input.close();
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Department No., name and location : ");
+        int dno = sc.nextInt();
+        String dname = sc.next(), dloc = sc.next();
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost/test", "root", "");
+        PreparedStatement pstmt = null;
+        String query = "INSERT INTO dept(deptno, dname, location)" + "VALUES (?, ?, ?)";
+        pstmt = con.prepareStatement(query);
+        pstmt.setInt(1, dno);
+        pstmt.setString(2, dname);
+        pstmt.setString(3, dloc);
+        int status = pstmt.executeUpdate();
+        if (status > 0) System.out.println("Record is inserted successfully !!!");
     }
 }
 // Expected Output:
