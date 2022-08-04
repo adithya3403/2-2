571a96114c86c22f9ee4539bcc45946dad0ddd23
Adithya K <a.dty.3403@gmail.com>
2022-08-04 10:23:06 +0530

    Update q3c.java

--- JAVA/q3c.java
+++ JAVA/q3c.java
@@ -1,13 +1,11 @@
 // c. Given n non-negative integers representing an elevation map where the width
-// of each bar is 1,compute how much water it is able to trap after raining.
-// Examples:
-// Input : arr[] = {3, 0, 0, 2, 0, 4}
-// Output : 10
-// We can trap"3*2 units"of water between 3 an 2,"1 unit"on top of bar 2 and"3 units"between 2 and 4.
+// of each bar is 1, compute how much water it is able to trap after raining. Examples:
+// Input : arr[] = {3, 0, 0, 2, 0, 4} Output : 10
+// We can trap"3*2 units"of water between 3 an 2, "1 unit" on top of bar 2 and "3 units" between 2 and 4.
 import java.util.*;
 public class q3c {
-    public static int maxWater(int[] arr) {
-        int res = 0, n = arr.length;
+    public static int maxWater(int[] arr, int n) {
+        int res = 0;
         for (int i = 1; i < n - 1; i++) {
             int left = arr[i];
             for (int j = 0; j < i; j++) left = Math.max(left, arr[j]);
@@ -22,6 +20,6 @@ public class q3c {
         int n=sc.nextInt();
         int[] arr=new int[n];
         for (int i=0; i<n; i++) arr[i]=sc.nextInt();
-        System.out.print(maxWater(arr));
+        System.out.print(maxWater(arr, arr.length));
     }
 }
