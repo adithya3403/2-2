257da5821f1911067fa6c9ef77f960a70f042058
Adithya K <a.dty.3403@gmail.com>
2022-08-04 11:19:55 +0530

    Update q1b.java

--- JAVA/q1b.java
+++ JAVA/q1b.java
@@ -3,29 +3,15 @@
 // digits long and is divisible by the number formed by
 // stringing the first and last numbers together.The smallest
 // number that fits this description is 100. First digit is 1,
-// last digit is 0,forming 10,which is a factor of 100.
-// Therefore,100 is gupful.
+// last digit is 0,forming 10,which is a factor of 100. Therefore, 100 is gupful.
 import java.util.*;
 public class q1b {
-    private static boolean isGupful(int n) {
-        String s = "" + n;
-        int div = Integer.parseInt("" + s.charAt(0) + s.charAt(s.length() - 1));
-        return n % div == 0;
-    }
-    public static int gupful(int n) {
-        for (int i = 0; n - i > 100; i++) {
-            boolean low = isGupful(n - i);
-            boolean high = isGupful(n + i);
-            if (low || high) return low ? n - i : n + i;
-        }
-        return 100;
-    }
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        System.out.println(gupful(sc.nextInt()));
+        int n=sc.nextInt();
+        if (n>=100 && n%(n/10+n%10)==0) System.out.println(true);
+        else System.out.println(false);
     }
 }
-// input=25
-// output=100
-// input=103
-// output=105
+// input: 103 output: false
+// input: 105 output: true
