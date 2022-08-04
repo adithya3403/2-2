0e2b53ba33c04e0ca785df5a51c9dda030cbfd5a
Adithya K <a.dty.3403@gmail.com>
2022-08-04 08:17:28 +0530

    Update q_10b.java

--- JAVA/q_10b.java
+++ JAVA/q_10b.java
@@ -49,35 +49,18 @@
 // 3 rows in set (0.00 sec)
 import java.sql.*;
 public class q_10b {
-    public static void main(String args[]) {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/test", "root", "");
-            Statement stmt = con.createStatement();
-            String sql = "select ename from emp e, dept d  where e.deptno=d.deptno and location = 'Dallas'";
-            ResultSet rs = stmt.executeQuery(sql);
-            ResultSetMetaData rsmd = rs.getMetaData();
-            System.out.println("");
-            int numberOfColumns = rsmd.getColumnCount();
-            for (int i = 1; i <= numberOfColumns; i++) {
-                if (i > 1) System.out.print("	");
-                String columnName = rsmd.getColumnName(i);
-                System.out.print(columnName);
-            }
-            System.out.println("");
-            while (rs.next()) {
-                for (int i = 1; i <= numberOfColumns; i++) {
-                    if (i > 1) System.out.print(",  ");
-                    String columnValue = rs.getString(i);
-                    System.out.print(columnValue);
-                }
-                System.out.println("");
-            }
-            con.close();
-        } catch (Exception e) { System.out.println(e); }
+    public static void main(String args[]) throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost/test", "root", "");
+        Statement stmt = con.createStatement();
+        String sql = "select ename from emp e, dept d  where e.deptno=d.deptno and location = 'Dallas'";
+        ResultSet rs = stmt.executeQuery(sql);
+        ResultSetMetaData rsmd = rs.getMetaData();
+        int n = rsmd.getColumnCount();
+        for (int i = 1; i <= n; i++) System.out.print(rsmd.getColumnName(i));
+        while (rs.next()) for (int i = 1; i <= n; i++) System.out.print("\n" + rs.getString(i));
     }
 }
-// Expected Output :
 // ename
 // Nitesh
 // Mahesh
